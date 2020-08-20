73b697c9a30917bfdb188efd88d3fe2db0eb8bc1
Rubin <rubinradhakrishnan@gmail.com>
2020-08-20 23:30:46 +0530

    Add detail view for each blog post.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # 127.0.0.1:8000
     path('', views.post_list, name='post_list'),
+    # 127.0.0.1:8000/post/1
+    path('post/<int:pk>/', views.post_detail, name='post_detail')
 ]
