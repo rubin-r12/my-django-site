547665e549dcb4b5fe9435232646ccf70b4cc6d7
Rubin <rubinradhakrishnan@gmail.com>
2020-08-21 15:05:29 +0530

    Add feature to edit blog posts from frontend.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('post/<int:pk>/', views.post_detail, name='post_detail'),
     # 127.0.0.1:8000/post/new
     path('post/new/', views.post_new, name='post_new'),
+    # 127.0.0.1:8000/post/1/edit
+    path('post/<int:pk>/edit/', views.post_edit, name='post_edit'),
 ]
