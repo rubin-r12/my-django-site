a800145285fb2d37394b3e9ce5b25f84a9107a3d
Rubin <rubinradhakrishnan@gmail.com>
2020-08-21 13:14:38 +0530

    Add feature to create new blog posts from frontend.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     # 127.0.0.1:8000
     path('', views.post_list, name='post_list'),
     # 127.0.0.1:8000/post/1
-    path('post/<int:pk>/', views.post_detail, name='post_detail')
+    path('post/<int:pk>/', views.post_detail, name='post_detail'),
+    # 127.0.0.1:8000/post/new
+    path('post/new/', views.post_new, name='post_new'),
 ]
