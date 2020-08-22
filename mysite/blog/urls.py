2dabcb1a3600c52d3f36bc751cdd603871d6cb50
Rubin <rubinradhakrishnan@gmail.com>
2020-08-22 20:58:50 +0530

    Add feature for new users to signup.

--- mysite/blog/urls.py
+++ mysite/blog/urls.py
@@ -22,4 +22,6 @@ urlpatterns = [
     path('comment/<int:pk>/remove/', views.comment_remove, name='comment_remove'),
     # 127.0.0.1:8000/comment/1/approve
     path('comment/<int:pk>/approve/', views.comment_approve, name='comment_approve'),
+    # 127.0.0.1:8000/signup
+    path('signup/', views.signup, name='signup'),
 ]
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = '(kp8p*a)p(mw*=o!x)flgq1i1^jl=hj=*y%fdj*verq39c$m*_'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 ALLOWED_HOSTS = []
 
 LOGIN_REDIRECT_URL = 'post_list'
@@ -40,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'blog',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
